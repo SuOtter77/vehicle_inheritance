87cec5724e1dc643c3b8d831f3c9f51cf25bc470
AG <186358781+SuOtter77@users.noreply.github.com>
2024-12-01 22:55:35 -0500

    Added Truck derived class declarations stuff...

--- Truck.h
+++ Truck.h
@@ -1,1 +1,14 @@
 #pragma once
+#ifndef TRU_HEADER
+#define TRU_HEADER
+#include "Vehicle.h" // includes <string>
+
+class Truck : public Vehicle {
+	int towingCapacity;
+public:
+	Truck(std::string manufacturerName, int year, int towingCap);
+	int getTowingCapacity();
+	void displayInfo(); // call Vehicle displayInfo
+};
+
+#endif
