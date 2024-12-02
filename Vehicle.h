3aa27e7b5b6c3976779e2de698481986ebbcb7b6
AG <186358781+SuOtter77@users.noreply.github.com>
2024-12-01 22:53:17 -0500

    Added general class (vehicle)

--- Vehicle.h
+++ Vehicle.h
@@ -1,1 +1,19 @@
 #pragma once
+#ifndef VEH_HEADER
+#define VEH_HEADER
+#include <string>
+#include <iostream>
+
+class Vehicle {
+	std::string manufacturer;
+	int yearBuilt;
+
+public:
+	Vehicle(std::string manufacturerName, int year); // implementation in Vehicle.cpp
+	std::string getManufacturer();
+	int getYear();
+	void displayInfo();
+	// Setters would be needed if we weren't setting all values in the constructor
+};
+
+#endif
