01bb5a80f52ae7e013c8541f9be9910e47d73229
AG <186358781+SuOtter77@users.noreply.github.com>
2024-12-01 22:54:53 -0500

    Added car derived class

--- Car.h
+++ Car.h
@@ -1,1 +1,14 @@
 #pragma once
+#ifndef CAR_HEADER
+#define CAR_HEADER
+#include "Vehicle.h" // includes <string>
+
+class Car : public Vehicle {
+	int numDoors;
+public:
+	Car(std::string manufacturerName, int year, int numberDoors);
+	int getNumDoors();
+	void displayInfo(); // call Vehicle displayInfo
+};
+
+#endif
